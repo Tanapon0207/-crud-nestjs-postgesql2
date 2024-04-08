255e5316d25bdad5905b650f420cdad5e63986c4
Tanapon Rattanamano <80101335+Tanapon0207@users.noreply.github.com>
2024-04-08 17:04:35 +0700

    new-update (8/4/2024 17:04)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ForgetpasswordComponent } from './Components/forgetpassword/forgetpassword.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'forgetpassword',
+    component: ForgetpasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ForgetpasswordComponent } from './Components/forgetpassword/forgetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    ForgetpasswordComponent
   ],
   imports: [
     BrowserModule,
